4384299f771a0177d87f91ce1369c9270950c3da
jihadkun <jihadkunnaji@gmail.com>
2020-08-20 17:29:35 +0700

    added form on editpage

--- src/EditPage.js
+++ src/EditPage.js
@@ -1,52 +1,80 @@
 import React from "react";
-import style from "./EditPage.Module.css";
+import "./EditPage.Module.css";
 
 class EditPage extends React.Component {
   render() {
     return (
-      <div className={style.container}>
-        <div className={style.row}>
-          <div className={style.fieldLabel}>Nama Mempelai Pria</div>
+      <form>
+        <div>
+          <p>Informasi Mempelai</p>
           <div>
-            <input type="text" />
+            <input type="text" placeholder="Pria" />
+            <input type="text" placeholder="Wanita" />
+          </div>
+          <div>
+            <input type="text" placeholder="Keluarga Pria" />
+            <input type="text" placeholder="Keluarga Wanita" />
           </div>
         </div>
 
-        <div className={style.row}>
-          <div className={style.fieldLabel}>Nama Mempelai Wanita</div>
+        <div>
+          <p>Informasi Resepsi</p>
           <div>
-            <input type="text" />
+            <input type="text" placeholder="Tanggal" />
+            <input type="text" placeholder="waktu" />
+            <input type="text" placeholder="lokasi" />
           </div>
-        </div>
 
-        <div className={style.row}>
-          <div className={style.fieldLabel}>Nama Keluarga Pria</div>
           <div>
-            <input type="text" />
+            <p>Cerita Pertemuan Mempelai</p>
+            <textarea placeholder="Silakan ceritakan apapun tentang kedua mempelai" />
           </div>
         </div>
+      </form>
 
-        <div className={style.row}>
-          <div className={style.fieldLabel}>Nama Keluarga Wanita</div>
-          <div>
-            <input type="text" />
-          </div>
-        </div>
+      // <div className="container">
+      //   <div className="row">
+      //     <div className="fieldLabel">Nama Mempelai Pria</div>
+      //     <div>
+      //       <input type="text" />
+      //     </div>
+      //   </div>
 
-        <div className={style.row}>
-          <div className={style.fieldLabel}>Kronologi Pertemuan</div>
-          <div>
-            <textarea type="text" />
-          </div>
-        </div>
+      //   <div className="row">
+      //     <div className="fieldLabel">Nama Mempelai Wanita</div>
+      //     <div>
+      //       <input type="text" />
+      //     </div>
+      //   </div>
 
-        <div className={style.row}>
-          <div className={style.fieldLabel}></div>
-          <div>
-            <button>"Simpan"</button>
-          </div>
-        </div>
-      </div>
+      //   <div className="row">
+      //     <div className="fieldLabel">Nama Keluarga Pria</div>
+      //     <div>
+      //       <input type="text" />
+      //     </div>
+      //   </div>
+
+      //   <div className="row">
+      //     <div className="fieldLabel">Nama Keluarga Wanita</div>
+      //     <div>
+      //       <input type="text" />
+      //     </div>
+      //   </div>
+
+      //   <div className="row">
+      //     <div className="fieldLabel">Kronologi Pertemuan</div>
+      //     <div>
+      //       <textarea type="text" />
+      //     </div>
+      //   </div>
+
+      //   <div className="row">
+      //     <div className="fieldLabel"></div>
+      //     <div>
+      //       <button>"Simpan"</button>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
